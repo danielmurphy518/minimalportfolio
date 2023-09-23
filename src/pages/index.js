63cdc9d7ab6df12b76dbab23a87cabc84ef75b24
danielmurphy518 <daniel.murphyx@gmail.com>
2023-09-23 19:46:28 +1200

    images

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,10 +17,9 @@ export default function IndexPage() {
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
         <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
-        <AboutSection sectionId="about" heading="About Portfolio Minimal" />
-        <InterestsSection sectionId="details" heading="Details" />
-        <ProjectsSection sectionId="features" heading="Built-in Features" />
-        <ContactSection sectionId="github" heading="Issues?" />
+        <AboutSection sectionId="about" heading="About Me" />
+        <InterestsSection sectionId="details" heading="Skills" />
+        <ProjectsSection sectionId="features" heading="Projects" />
       </Page>
     </>
   );
